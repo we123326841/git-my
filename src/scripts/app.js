ecd6e1a520198a910f8c462e79633066f9f57dfa
we123326841 <wanghao@wanghaodeMacBook-Pro-3.local>
2017-04-21 15:37:18 +0800

    第一次提交代码到本地仓库

---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wanghao on 17/4/21.
+ */
+var wafwa = 21;
+
+(function (a) {
+    console.log(a);
+})(21)
---
+++ src/scripts/demo.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wanghao on 17/4/21.
+ */
+console.log(1111);
